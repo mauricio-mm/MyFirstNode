e271beb69c5383b8d3c9db02fa85b7f30d50e0e0
mauricio-mm <mauricio.moura@acad.pucrs.br>
2024-06-30 22:13:31 -0300

    Add Tailwind in Project

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(routes);
 
 app.listen(process.env.port || 3000);
